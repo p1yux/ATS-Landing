de66cc6a7972bca2e43fbae0c8248eb141f86156
p1yux <soulpiyush09@gmail.com>
2025-03-21 12:50:42 +0530

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       <NavBar />
       <HeroSection />
       {/* <Companies /> */}
-      <VideoPlayer />
+      {/* <VideoPlayer /> */}
       <ATSFor />
       <Pills />
       <About />
